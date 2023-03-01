63e56018e007bfbbea35e1521e8300ffe158030f
Mbonneton <109339120+Mbonneton@users.noreply.github.com>
2023-03-01 01:01:12 +0100

    evo

--- front/js/product.js
+++ front/js/product.js
@@ -10,11 +10,10 @@ function main (){
     .catch((err) => {
         alert("l'id du produit est incorrect.");
     })
-    //*Si on a bien récupéré un id on récupère les données de l'API correspondant à cet id
+    //*Si on a bien récupéré un id on récupère les données de l'API correspondant à cet id-
 }
 function contenue (selectionproduit){
-
-      //* ajout prix / titre / description et couleur du produits
+    //* ajout prix / titre / description et couleur du produits
     const img = document.createElement("img");
     img.src = selectionproduit.imageUrl;
     img.alt = selectionproduit.altTxt;
@@ -24,56 +23,54 @@ function contenue (selectionproduit){
     document.getElementById("description").innerText = selectionproduit.description;
     const select = document.getElementById("colors");
     selectionproduit.colors.forEach(function (color) {
-        const option = document.createElement("option");
+    const option = document.createElement("option");
         option.value = color;
         option.innerText = color;
-            select.appendChild(option);// creation d'un enfant 
+        select.appendChild(option);// creation d'un enfant 
         })
-               //*Sélection du bouton Ajouter au panier.
+        //*Sélection du bouton Ajouter au panier.
     const selectBoutonPanier = document.querySelector("#addToCart");
-                 //*Ecoute du bouton Panier pour envoyer les choix de l'utilisateur
+    //*Ecoute du bouton Panier pour envoyer les choix de l'utilisateur
     selectBoutonPanier.addEventListener("click", (event)=>{
         events(event,selectionproduit)
     });
 }
 if (productId !== null){
-
-    main()
+ main()
 }else{
     alert(`Le produit sélectionné n'a pas été trouvé !`);
     window.location.href = "index.html";
 }
 function events (event,selectionproduit){
-  event.preventDefault();
+    event.preventDefault();
                  //*Sélection de l'id pour le choix de la couleur
-  const colorId = document.querySelector("#colors");
+    const colorId = document.querySelector("#colors");
                  //*insertion de la couleur choisie par l'utilisateur dans une variable
-  choiceColor = colorId.value;
+    choiceColor = colorId.value;
                  //*Sélection de l'id pour le choix de la quantité et insertion de la quantité choisie par l'utilisateur dans une variable
-  const quantity = document.querySelector("#quantity");
-  choiceQuantity = Number(quantity.value);
-                 //*Récupération des données (id, couleur et quantité) après les choix faits par l'utilisateur.
-                 //*condition que la couleur soit bien sélectionnée.
-                 //*et que la quantité indiquée par l'utilisateur soit comprise entre 1 et 100.
-                 //*et que la quantité entrée par l'utilisateur soit un nombre entier.
-  if (choiceColor !== "" && choiceQuantity > 0 && choiceQuantity <= 100 && Number.isInteger(choiceQuantity)) {
+    const quantity = document.querySelector("#quantity");
+    choiceQuantity = Number(quantity.value);
+//*Récupération des données (id, couleur et quantité) après les choix faits par l'utilisateur.
+//*condition que la couleur soit bien sélectionnée.
+//*et que la quantité indiquée par l'utilisateur soit comprise entre 1 et 100.
+//*et que la quantité entrée par l'utilisateur soit un nombre entier.
+    if (choiceColor !== "" && choiceQuantity > 0 && choiceQuantity <= 100 && Number.isInteger(choiceQuantity)) {
     let optionsProduct = {
         idProduct: selectionproduit._id ,
         colorProduct: choiceColor ,
         quantityProduct: choiceQuantity
     }
-                   //*Déclaration de la variable "produitEnregistreDansLocalStorage" dans laquelle on récupère les keys et les values.
-                //*et qui sont dans le localStorage afin de contrôler si le localStorage est vide ou non
+//*Déclaration de la variable "produitEnregistreDansLocalStorage" dans laquelle on récupère les keys et les values.
+//*et qui sont dans le localStorage afin de contrôler si le localStorage est vide ou non
     let produitEnregistreDansLocalStorage = JSON.parse(localStorage.getItem("produit"));
-
-                    //* produit si nul va alors crée un tableau vide.
+    //* produit si nul va alors crée un tableau vide.
     if (produitEnregistreDansLocalStorage==null) {produitEnregistreDansLocalStorage=[]}
-                     //*Si le produit et la couleur choisis existent déjà dans le localStorage alors on incrémente uniquement la quantité
+//*Si le produit et la couleur choisis existent déjà dans le localStorage alors on incrémente uniquement la quantité
         let findProduct = produitEnregistreDansLocalStorage.find((x) => {return x.idProduct === optionsProduct.idProduct && x.colorProduct === optionsProduct.colorProduct});
     if(findProduct){
         const total = Number(findProduct.quantityProduct) + Number(optionsProduct.quantityProduct);
         if(total <= 100){
-                    //*On met la variable alerte sur false pour pouvoir afficher un message plus approprié    
+ //*On met la variable alerte sur false pour pouvoir afficher un message plus approprié    
             findProduct.quantityProduct = Number(findProduct.quantityProduct) + Number(optionsProduct.quantityProduct);
             alert(`La quantité du produit ${selectionproduit.name} de couleur ${choiceColor} a bien été mise à jour.`);
         }else{ //*On met la variable alert sur false pour pouvoir afficher un message plus approprié
@@ -84,7 +81,7 @@ function events (event,selectionproduit){
                         //*On met la variable alerte sur false pour pouvoir afficher un message plus approprié
         alert(`Le produit ${selectionproduit.name} de couleur ${choiceColor} a bien été ajouté au panier.`);
     }
-               //*Transformation en format JSON et envoi des infos dans la clé "produit" du localStorage
+ //*Transformation en format JSON et envoi des infos dans la clé "produit" du localStorage
     localStorage.setItem("produit", JSON.stringify(produitEnregistreDansLocalStorage))
 }else { //*On met la variable alerte sur false pour pouvoir afficher un message plus approprié en rapport à la séléction de quantitée.
     alert(`La couleur n'est pas sélectionnée et/ou la quantité n'est pas comprise entre 1 et 100 ou n'est pas un nombre entier. Veuillez vérifier !`);
